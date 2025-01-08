e41f41e1a6c99d152ae389ec9357018d73286ceb
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-01-08 21:20:20 +0300

    Add files via upload

---
+++ Ortalama-Hesaplama/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ortalama_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ortalama;
+            ortalama = Convert.ToInt32(textBox1.Text);
+            if (ortalama > 70 && ortalama < 85) 
+            {
+                MessageBox.Show("teşekkür belgesi");
+            }
+            if (ortalama > 84 && ortalama < 100)
+            {
+                MessageBox.Show("takdir belgesi");
+            }
+            if (ortalama > 0 && ortalama < 49)
+            {
+                MessageBox.Show("kaldınız");
+            }
+            if (ortalama > 50 && ortalama < 70) 
+            {
+                MessageBox.Show("belgesiz");
+            }
+        }
+    }
+}
---
+++ Ortalama-Hesaplama/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Ortalama-Hesaplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ortalama-Hesaplama")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("3985c533-270f-4e83-91c2-ea2a19279d4b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
