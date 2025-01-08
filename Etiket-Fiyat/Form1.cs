f9349d4299e4dda758642240b6e99b5b65fe114d
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-01-08 20:29:19 +0300

    Add files via upload

---
+++ Etiket-Fiyat/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Etiket_Fiyat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int etiketfiyatı;
+        double indirimFiyat;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            etiketfiyatı = Convert.ToInt32(textBox1.Text);
+            indirimFiyat = etiketfiyatı - etiketfiyatı * 10 / 100;
+            label1.Text = indirimFiyat.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            etiketfiyatı = Convert.ToInt32(textBox1.Text);
+            indirimFiyat = etiketfiyatı - etiketfiyatı * 25 / 100;
+            label1.Text = indirimFiyat.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            etiketfiyatı = Convert.ToInt32(textBox1.Text);
+            indirimFiyat = etiketfiyatı - etiketfiyatı * 50 / 100;
+            label1.Text = indirimFiyat.ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            etiketfiyatı = Convert.ToInt32(textBox1.Text);
+            indirimFiyat = etiketfiyatı - etiketfiyatı * 100 / 100;
+            label1.Text = indirimFiyat.ToString();
+
+        }
+    }
+}
---
+++ Etiket-Fiyat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Etiket-Fiyat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Etiket-Fiyat")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("d60987ca-97cf-4814-bac6-d95f7a5c27c0")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
