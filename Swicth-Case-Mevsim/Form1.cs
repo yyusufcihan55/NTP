921c897084b227408646fdadadada0c08dd92858
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-01-08 22:12:26 +0300

    Add files via upload

---
+++ Swicth-Case-Mevsim/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swicth_Case_Mevsim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mevsim = Convert.ToInt32(textBox1.Text);
+
+            switch (mevsim) 
+            {
+                case 1:
+                    this.BackColor = Color.Yellow;
+                    MessageBox.Show("yaz");
+                    break;
+                case 2:
+                    this.BackColor = Color.LightGreen;
+                    MessageBox.Show("ilk bahar");
+                    break;
+                case 3:
+                    this.BackColor = Color.Orange;
+                    MessageBox.Show("son bahar");
+                    break;
+                case 4:
+                    this.BackColor = Color.White;
+                    MessageBox.Show("kış");
+                    break;
+                default:
+                    MessageBox.Show("hata");
+                    break;
+            }
+        }
+    }
+}
---
+++ Swicth-Case-Mevsim/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Swicth-Case-Mevsim")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Swicth-Case-Mevsim")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("2e892602-6390-492f-9620-f565ffcf69f6")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
