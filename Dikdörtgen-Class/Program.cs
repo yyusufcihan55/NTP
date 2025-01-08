5973fc4a39ccaab717eb66a1262f43e5ac09c538
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-01-08 23:26:45 +0300

    Add files via upload

---
+++ Dikdörtgen-Class/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Dikdörtgen_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dıkdortgen d = new dıkdortgen(3,4);
+            Console.WriteLine("dikdörtgenin alanı :" + d.alanhesapla());
+            Console.WriteLine("dikdörtgenin çevresi :" + d.cevrehesapla());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dikdörtgen-Class/dıkdortgen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dikdörtgen_Class
+{
+    internal class dıkdortgen
+    {
+        private int a, b;
+        public dıkdortgen(int a,int b) 
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int alanhesapla() 
+        {
+            return a * b;
+        }
+        public int cevrehesapla() 
+        {
+            return 2*(a + b);
+        }
+
+
+    }
+}
