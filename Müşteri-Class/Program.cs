045f0b5190c3560c824e3b0a95b9a2ab34c7bb9e
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-01-08 23:28:08 +0300

    Add files via upload

---
+++ Müşteri-Class/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Müşteri_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
