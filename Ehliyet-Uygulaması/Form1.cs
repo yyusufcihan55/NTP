e2f2f7028830b5ec9ea4c24f0498e8063050898b
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-01-08 20:27:16 +0300

    Add files via upload

---
+++ Ehliyet-Uygulaması/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ehliyet_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yas;
+            yas = Convert.ToInt32(textBox1.Text);
+            if (yas > 17)
+            {
+                MessageBox.Show("ehliyet alabilirsiniz");
+            }
+            else 
+            {
+                MessageBox.Show("yaşınız tutmuyor");
+            }
+        }
+    }
+}
