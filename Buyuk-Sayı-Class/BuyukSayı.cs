61257470638af1b0e9a014396d57bd8015ab1d63
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-02-06 17:57:16 +0300

    Add files via upload

---
+++ Buyuk-Sayı-Class/BuyukSayı.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buyuk_Sayı_Class
+{
+    internal class BuyukSayı
+    {
+        public int BüyükSayiyiBul(int sayi1,int sayi2) 
+        {
+            string buyuksayi;
+            int sonuc;
+            if (sayi1 < sayi2)
+            {
+                sonuc = sayi2;
+            }
+            else 
+            {
+                sonuc = sayi1;
+            }
+            return sonuc;
+        }
+    }
+}
---
+++ Buyuk-Sayı-Class/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Buyuk_Sayı_Class
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            buyuksayi buyuk = new buyuksayi();
+            int a = 100;
+            int b = 250;
+            int sonuc = buyuk.büyüksayiyibul(a, b);
+            Console.WriteLine("büyük olan sayı"+sonuc);
+            Console.ReadLine();
+        }
+    }
+}
