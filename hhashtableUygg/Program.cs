634593669bf1d0aeecc0a10c45b8122436999dfd
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-05-21 21:40:25 +0300

    Add files via upload

---
+++ hhashtableUygg/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace hhashtableUygg
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Hashtable ogrenciler = new Hashtable();
+
+            ogrenciler.Add(43, "cihan");
+            ogrenciler.Add("yusuf", 72);
+            ogrenciler.Add("rüzgar",147);
+            ogrenciler.Add("burak", 107);
+
+            ogrenciler.Remove(43);
+            ogrenciler.Remove("burak");
+
+            ogrenciler[147] = "yusuf";
+            ogrenciler["rüzgar"] = 145;
+
+            foreach (var anahtar in ogrenciler.Values) 
+            {
+                Console.WriteLine(anahtar);
+            }
+            foreach (DictionaryEntry eleman in ogrenciler)
+            {
+                Console.WriteLine(eleman.Key+"---"+eleman.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
