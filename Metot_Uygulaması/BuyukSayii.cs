37b5ce3b070af59dd1cebcd6b4222a2e838edad2
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-05-21 21:43:09 +0300

    Add files via upload

---
+++ Metot_Uygulaması/BuyukSayii.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metot_Uygulaması
+{
+    internal class BuyukSayii
+    {
+        public int buyuksayiyibul(int sayi1,int sayi2) 
+        {
+            int sonuç;
+            if (sayi1 > sayi2)
+            {
+                sonuç = sayi2;
+            }
+            else
+            {
+                sonuç = sayi1;
+            }
+            return sonuç;
+        }
+    }
+}
---
+++ Metot_Uygulaması/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Metot_Uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            buyuksayi buyuk = new buyuksayi();
+            int a = 100;
+            int b = 25;
+            int sonuç = buyuk.buyuksayiyibul(a, b);
+            Console.WriteLine("büyük olsan sayı"+sonuç);
+            Console.ReadLine();
+        }
+    }
+}
