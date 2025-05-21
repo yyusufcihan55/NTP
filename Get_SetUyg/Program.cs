5aed1cb601d3b94dde33efae5e03c030ae7c84bf
Yusuf Cihan ÜÇÜNCÜ <tableticindenememail@gmail.com>
2025-05-21 21:39:25 +0300

    Add files via upload

---
+++ Get_SetUyg/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Get_SetUyg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            araba araba1 = new araba();
+            araba1.Setrenk("Mavi");
+            Console.WriteLine("arabanın rengi"+araba1.getrenk());
+
+            araba araba2 = new araba();
+            araba2.Setrenk("yeşil");
+            Console.WriteLine("arabanın rengi" + araba2.getrenk());
+
+            Console.ReadLine();
+        }
+    }
+}
